b7e84bc3ab4cba951d4a7648c7ee93afb7fcee19
idonia <sidoniag@gmail.com>
2020-11-03 20:57:10 -0600

    add about

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+            <h1>Hello, my name is Sarah Idonia Garrett.</h1>
+      <br />
+      <p>I am an artist, fullstack over-thinker and moving into development.</p>     
+       <p><i>I'm the Doctor. Well, they call me the Doctor. I don't know why. I call me the Doctor too.
+        I still don't know why. *Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke,
+        tell me what normal blokes do!</i>
+      </p >
+        </section>
+    );
+}
+
+export default About;
