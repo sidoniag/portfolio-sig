86e5de5ca08023d8ca43551b392049eacd1316c9
idonia <sidoniag@gmail.com>
2020-11-04 00:12:43 -0600

    add contact page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 function App() {
   const [categories] = useState([
     { name: "about me", href: "about" },
     { name: "portfolio", href: "a few of my projects" },
-    // { name: "contact", description: "ways to reach out to me" },
+    { name: "contact", description: "ways to reach out to me" },
     { name: "resume", href: "resume" },
   ]);
 
@@ -22,8 +23,8 @@ function App() {
     switch (currentCategory.name) {
       case "portfolio":
         return <Portfolio />;
-      // case "contact":
-      //   return <Contact />;
+      case "contact":
+        return <Contact />;
       case "resume":
         return <Resume />;
       default:
