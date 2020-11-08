df332ddcb3c839ef62eec9b3595a643f8f8e4d0a
idonia <sidoniag@gmail.com>
2020-11-08 00:08:26 -0600

    add another project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,31 +5,40 @@ import { Box } from "@chakra-ui/core";
 function Portfolio() {
   const [projects] = useState([
     {
-      name: "SootheR",
+      name: "TeachMeTo",
       imgUrl:
-        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/soother.png",
+        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/TeachMeTo.png",
       description:
-        "Bathed in soothing colors, SootheR is an application where the user can search and read news articles while getting some encouraging words across the top of their screens.",
-      link: "https://devmadia.github.io/soother/",
-      repo: "https://github.com/Devmadia/sootheR",
+        "TeachMeTo is an application built with React. It is a place where users can find and contribute knowledge. Users can donate to contributors whose resources they admire.",
+      link: "https://floating-spire-39046.herokuapp.com/",
+      repo: "https://github.com/evkonradi/teach-me-to",
     },
     {
       name: "CardSocial",
       imgUrl:
-        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/cardsocial.png",
+        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/CardSocial.png",
       description:
         "Card Social is an application for users that may have multiple hussles and want to connect. Create a unique card to share with others via QRcode.",
       link: "https://cryptic-shelf-96558.herokuapp.com/",
       repo: "https://github.com/evkonradi/cardsocial",
     },
     {
-      name: "TeachMeTo",
+      name: "SootheR",
       imgUrl:
-        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/teachmeto.png",
+        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/Soother.png",
+      description:
+        "Bathed in soothing colors, SootheR is an application where the user can search and read news articles while getting some encouraging words across the top of their screens.",
+      link: "https://devmadia.github.io/soother/",
+      repo: "https://github.com/Devmadia/sootheR",
+    },
+    {
+      name: "TrackMyBudget",
+      imgUrl:
+        "https://raw.githubusercontent.com/sidoniag/portfolio-sig/master/src/assets/projects/TrackMyBudget.png",
       description:
         "TeachMeTo is an application built with React. It is a place where users can find and contribute knowledge. Users can donate to contributors whose resources they admire.",
-      link: "https://floating-spire-39046.herokuapp.com/",
-      repo: "https://github.com/evkonradi/teach-me-to",
+      link: "https://limitless-sierra-65574.herokuapp.com/",
+      repo: "https://github.com/sidoniag/trackmybudget",
     },
   ]);
 
