b70440ea47f736d65574c5ab7d1147426fff5ab6
idonia <sidoniag@gmail.com>
2020-11-27 15:12:31 -0600

    plain style

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -30,9 +30,9 @@ function ContactForm() {
       <a href="https://www.linkedin.com/in/sarahidoniagarrett/" target="_blank" rel="noopener noreference"><img src={require(`../../assets/linkedin_logo.png`)} alt="linkedin" className="icon" /></a>
       <a href="mailto:seekersig@gmail.com" target="_blank" rel="noopener noreference"><img src={require(`../../assets/email_icon.jpeg`)} alt="email" className="icon" /></a> */}
       <a href="https://spark.adobe.com/page/wGm9zXRSbZMYL/">art portfolio</a>
+      <a href="mailto:seekersig@gmail.com">email</a>
       <a href="https://github.com/sidoniag">github</a>
       <a href="https://www.linkedin.com/in/sarahidoniagarrett/">linkedin</a>
-      <a href="mailto:seekersig@gmail.com">email</a>
   
     </Flex>
       </Box>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "reactstrap";
 import Fab from '@material-ui/core/Fab';
-import NavigationIcon from '@material-ui/icons/Navigation';
+import { NavigationIcon, AppBar, Toolbar } from '@material-ui/icons/Navigation';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,21 +20,27 @@ function Footer() {
 
   return (
     <div>
-      <Container className="footer">
+        {/* <React.Fragment>
+      <AppBar position="fixed"> */}
+    {/* //     <Toolbar>{ content }</Toolbar>*/
+    //   </AppBar>
+    //   <Toolbar />
+    // </React.Fragment>  */}
+      // <Container className="footer" position="sticky">
       
         {/* <a href="https://github.com/sidoniag" target="_blank" rel="noopener noreference"><img src={require(`../../assets/github_logo.png`)} alt="github" className="icon" /></a>
         <a href="https://www.linkedin.com/in/sarahidoniagarrett/" target="_blank" rel="noopener noreference"><img src={require(`../../assets/linkedin_logo.png`)} alt="linkedin" className="icon" /></a>
         <a href="mailto:seekersig@gmail.com" target="_blank" rel="noopener noreference"><img src={require(`../../assets/email_icon.jpeg`)} alt="email" className="icon" /></a> */}
-{/* 
+/* 
         <a href="https://github.com/sidoniag" target="_blank" rel="noopener noreference">github</a>
         <a href="https://www.linkedin.com/in/sarahidoniagarrett/" target="_blank" rel="noopener noreference">linkedin</a>
         <a href="mailto:seekersig@gmail.com" target="_blank" rel="noopener noreference">email</a> */}
 
 <Fab>
-  <NavigationIcon />
+  <NavigationIcon variant="round"/>
 </Fab>
-      </Container>
-    </div>
+      {/* </Container> */}
+</div>
   );
 }
 
